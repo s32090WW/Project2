a113d1c2142f99189dd2ab9adff9a1284f466ea9
s32090WW <s32090@pjwstk.edu.pl>
2025-06-02 06:30:16 +0200

    02.06.2025

--- Assets/Cainos/Script/Collectible.cs
+++ Assets/Cainos/Script/Collectible.cs
@@ -10,10 +10,14 @@ public class Collectible : MonoBehaviour
         {
             return;
         }
-        
-        Inventory playerInventory = null; 
-        playerInventory = collision.gameObject.GetComponent<Inventory>();
-        
+
+        Inventory playerInventory = collision.gameObject.GetComponent<Inventory>();
+
+        if (playerInventory != null)
+        {
+            playerInventory.AddCollectible(); 
+        }
+
         Destroy(gameObject);
     }
 }
--- Assets/Cainos/Script/Inventory.cs
+++ Assets/Cainos/Script/Inventory.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    //counter for collectibles
+    public int collectibleCount = 0;
+   // public string collectibleText;
+
+    public void AddCollectible()
+    {
+        collectibleCount++;
+        Debug.Log("Collected: " + collectibleCount);
+        
+        //if (collectibleText != null)
+         //   collectibleText.text = "Collectibles: " + collectibleCount;
+    }
 }
